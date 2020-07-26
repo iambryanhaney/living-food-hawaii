cdc28e36887f01d7a7b1862308e4d9cdd855e4cb
iambryanhaney <i.am.bryan.haney@gmail.com>
2020-07-25 23:38:15 -0500

    image zoom modal with image navigation

--- frontend/src/routes/ExampleOfferings.js
+++ frontend/src/routes/ExampleOfferings.js
@@ -1,9 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.css'
+
 import React, { Component } from 'react'
 
 import Card from 'react-bootstrap/Card'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import Modal from 'react-bootstrap/Modal'
+import Button from 'react-bootstrap/Button'
 
 const DISH_URL = 'http://localhost:3001/dishes'
 const TAG_URL = 'http://localhost:3001/tags'
@@ -29,6 +33,9 @@ export default class ExampleOfferings extends Component {
             tags: [],
             filterEnabledColor: '#34b7eb',
             filterDisabledColor: null,
+            showZoomModal: false,
+            dishIndex: null,
+            imageIndex: null,
         }
     }
 
@@ -87,12 +94,19 @@ export default class ExampleOfferings extends Component {
     }
     
     renderDishes = () => {
-        return this.state.filteredDishes.map(dish => 
-            dish.images.map(image => 
+        const filteredDishes = this.state.filteredDishes.map((dish, dishIndex) => 
+            dish.images.map((image, imageIndex) => 
                 <Card key={`dish${dish.id}image${image.id}`} style={{ width: '20%', margin: '20px' }}>
-                    <Card.Img variation="top" src={this.state.dishImages[image.path]} />
+                    <Card.Img style={{ cursor: 'pointer' }} variation="top" src={this.state.dishImages[image.path]} onClick={() => this.showZoomModal(dishIndex, imageIndex)} />
                 </Card>
             )
+        )
+        return filteredDishes.length > 0 ? filteredDishes : (
+            <>
+                <br />
+                There are no offerings that match your exact request. Please try changing your filters.
+                <br />
+            </>
         )
     }
 
@@ -110,13 +124,84 @@ export default class ExampleOfferings extends Component {
             </Col>
         )
     }
-    
 
+    showZoomModal = (dishIndex, imageIndex) => {
+        this.setState({ showZoomModal: true, dishIndex: dishIndex, imageIndex: imageIndex})
+    }
+    
+    closeZoomModal = () => {
+        this.setState({ showZoomModal: false, dishIndex: null, imageIndex: null})
+    }
+    
+    navImageRight = () => {
+        const filteredDishes = this.state.filteredDishes
+        const dishIndex = this.state.dishIndex
+        const imageIndex = this.state.imageIndex
+
+        if (imageIndex < filteredDishes[dishIndex].images.length - 1) {
+            this.setState({ imageIndex: imageIndex + 1 })
+        } else if (dishIndex < filteredDishes.length - 1) {
+            this.setState({ dishIndex: dishIndex + 1, imageIndex: 0 })
+        } else {
+            this.setState({ dishIndex: 0, imageIndex: 0 })
+        }
+    }
+
+    navImageLeft = () => {
+        const filteredDishes = this.state.filteredDishes
+        const dishIndex = this.state.dishIndex
+        const imageIndex = this.state.imageIndex
+
+        if (imageIndex > 0) {
+            this.setState({ imageIndex: imageIndex - 1 })
+        } else if (dishIndex > 0) {
+            this.setState({ dishIndex: dishIndex - 1, imageIndex: filteredDishes[dishIndex - 1].images.length - 1 })
+        } else {
+            this.setState({ dishIndex: filteredDishes.length - 1, imageIndex: filteredDishes[filteredDishes.length - 1].images.length - 1})
+        }
+    }
+
+    renderZoomModal = () => {
+        // return this.state.filteredDishes.length === 0 ? null : (
+        return this.state.dishIndex === null ? null : (
+            <Modal size='lg' show={this.state.showZoomModal} onHide={() => this.closeZoomModal()} keyboard={false} centered>
+                <Modal.Header style={{ backgroundColor: '#60c9f0', textAlign: 'center' }} closeButton>
+                    {/* <Modal.Title></Modal.Title> */}
+                    {this.state.filteredDishes[this.state.dishIndex].description}
+                </Modal.Header>
+                <Modal.Body style={{ overflow: 'scroll', backgroundColor: '#dcdcdc' }}>
+                    <Container>
+                        <Row>
+                            <Button style={{ backgroundColor: '#7b8487' }} onClick={() => this.navImageLeft()}>
+                                <Col xs={1} >    
+                                </Col>
+                                &lt;
+                            </Button>
+                            <Col xs={10}>
+                                <Card>
+                                    <Card.Img onClick={() => this.closeZoomModal()} variation="top" src={this.state.dishImages[this.state.filteredDishes[this.state.dishIndex].images[this.state.imageIndex].path]} />
+                                </Card>
+                            </Col>
+                            <Button style={{ backgroundColor: '#7b8487' }} onClick={() => this.navImageRight()}>
+                                <Col xs={1}>
+                                </Col>
+                                &gt;    
+                            </Button>
+                            
+                        </Row>
+                    </Container>
+                </Modal.Body>
+                <Modal.Footer style={{ backgroundColor: '#dcdcdc', textAlign: 'center' }}>
+                    { this.state.filteredDishes[this.state.dishIndex].tags.map(tag => `#${tag.name} `) }
+                </Modal.Footer>
+            </Modal>
+        )
+    }
 
     render() {
         return (
             <div >
-                <h1>Example Offerings page...</h1>
+                <h1>Example Offerings</h1>
                 <Container>
                     <Row>
                         <Col xs={12} sm={6} md={2} lg >
@@ -139,6 +224,7 @@ export default class ExampleOfferings extends Component {
                 <Container style={{  display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'center', alignItems: 'center', border: this.state.borders.black}}>
                     { this.renderDishes() }
                 </Container>
+                { this.renderZoomModal() }
             </div>
         )
     }
