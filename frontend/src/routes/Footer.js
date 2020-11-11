ef30886e4f5f79864bb6609951fb2354c965de47
iambryanhaney <i.am.bryan.haney@gmail.com>
2020-11-10 22:04:37 -0600

    styled services section

--- frontend/src/routes/Footer.js
+++ frontend/src/routes/Footer.js
@@ -5,7 +5,7 @@ import React from 'react'
 export default function Footer() {
     return (
         <div className="footer-banner" id="footer">
-            <footer className="footer">
+            <footer className="footer bg-dark">
             </footer>
         </div>
     )
